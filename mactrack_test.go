a72184f647eee65d3ec1bdcf282ef55f38e843d1
Gabel, Parker D <parkerdgabel@email.arizona.edu>
2019-11-10 17:52:36 -0700

    Added some basic stuff

---
+++ mactrack_test.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestMactrackStartsDatabase(t *testing.T) {
+	t.Error("Implement me!")
+}
--- src/mactrack_test.go
+++
@@ -1,9 +0,0 @@
-package mactrack
-
-import (
-	"testing"
-)
-
-func TestMacTrackIncorrectArgs(t *testing.T) {
-
-}
